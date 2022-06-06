754bc6a539c74062853f88f127227221cdb01df5
huytruongquang <kzyo5962@gmail.com>
2022-06-07 01:22:13 +0700

    set up template auth

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\ClassroomController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('layout.main');
@@ -10,3 +11,7 @@ Route::get('/', function () {
 
 Route::resource('/students', StudentController::class)->except(['show']);
 Route::resource('/classrooms', ClassroomController::class)->except(['show']);
+
+Route::get('/login', [UserController::class, 'login'])->name('login');
+
+Route::get('/register', [UserController::class, 'register'])->name('register');
