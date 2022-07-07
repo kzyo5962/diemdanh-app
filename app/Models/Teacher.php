cc7d04dab5d566d7e8b7ba4ba2be4d5faaf85e3c
kzyo5962 <kzyo5962@gmail.com>
2022-07-08 01:13:53 +0700

    Update Teacher.php

--- app/Models/Teacher.php
+++ app/Models/Teacher.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Teacher extends Model
 {
@@ -21,5 +22,10 @@ class Teacher extends Model
     public function supervisor(): BelongsTo
     {
         return $this->belongsTo(Supervisor::class, 'supervisor_id', 'id');
+    }
+
+    public function attendances(): BelongsToMany
+    {
+        return $this->belongsToMany(Attendance::class, 'teacher_id');
     }
 }
